bedfc53d492e479e105a72e155773ee02360a79a
hildansaputraa <158609743+hildansaputraa@users.noreply.github.com>
2024-09-09 10:45:40 +0700

    update kirim data, pengkondisian jika tidak ada data yang masuk maka tidak dikirim

--- main.py
+++ main.py
@@ -4,37 +4,6 @@ import json
 #import serial
 import time
 
-def stackImages(scale,imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        ver = hor
-    return ver
-
 #kirim koordinat
 #PORT = '/dev/ttyUSB0'
 #BAUD_RATE = 9600
@@ -52,6 +21,8 @@ center_x = width // 2
 center_y = height // 2
 
 Xgreen, Xyellow, Ygreen, Yyellow = 0, 0, 0, 0
+
+nilaiPrev = ""
 
 # Load kalibrasi warna
 try:
@@ -120,7 +91,7 @@ while True:
                 Ygreen = cYy - center_y
         
                 cv2.circle(img, (cXy, cYy), 5, (0, 0, 255), -1)
-                cv2.putText(img, f"({cXy}, {cYy})", (cXy, cYy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                cv2.putText(img, f"({Xgreen}, {Ygreen})", (cXy, cYy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
                 cv2.drawContours(img, [cntr], 0, (0, 0, 255), 2)
                
   
@@ -143,16 +114,23 @@ while True:
                 Yyellow = cYy - center_y
         
                 cv2.circle(img, (cXy, cYy), 5, (0, 0, 255), -1)
-                cv2.putText(img, f"({Xyellow}, {Yyellow})", (Xyellow, Yyellow), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                cv2.putText(img, f"({Xyellow}, {Yyellow})", (cXy, cYy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
                 cv2.drawContours(img, [cntr], 0, (0, 0, 255), 2)
 
     
+    cv2.imshow("Image", img)
+    cv2.imshow("Result Green", blurGreen)
+    cv2.imshow("Result Yellow", blurYellow)
+    
+    
+    
+    nilaiNew = f"{Xgreen}+{Ygreen}+{Xyellow}+{Yyellow}"
+    
+    # Bandingkan nilaiNew dengan nilaiPrev
+    if nilaiNew != nilaiPrev:
+        print(f"{Xgreen}+{Ygreen}+{Xyellow}+{Yyellow}")
+        nilaiPrev = nilaiNew  # Update nilaiPrev setelah perbandingan
 
-    imgStack = stackImages(0.6,([img,imgHSV],[blurGreen,blurYellow]))
-    cv2.imshow("Image Stack", imgStack)
-
-
-    print(Xgreen,"+",Xyellow,"+",Ygreen,"+",Yyellow)
     #ser.write(f"{Xgreen},{Xyellow},{Ygreen},{Yyellow}\n".encode('utf-8'))
     
     key = cv2.waitKey(1)
